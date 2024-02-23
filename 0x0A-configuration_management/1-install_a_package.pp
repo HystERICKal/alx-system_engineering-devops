ea562f46dc480764ae122a47f3b17201d17c56cf
HystERICKal <nyoroerick12@gmail.com>
2024-02-23 04:24:29 -0800

    Fix Errors

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Now Let's install a package
-
-package { 'flask':
+#!/usr/bin/pup
+# Now Let's Install a package
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
+
