2c3bd9ef675d5ba6ad25a9339e05db431f57b912
HystERICKal <nyoroerick12@gmail.com>
2024-04-09 16:47:29 +0000

    Task_0 fix errors_v3...

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-'''Reddit sub counter'''
+"""Reddit sub counter."""
 import requests
 from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    '''Reddit sub counter'''
+    """Reddit sub counter."""
     redditor = {'User-Agent': 'Mozilla/5.0'}
     the_link = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=redditor).json()
+                            .format(subreddit), headers=redditor).json()
     try:
         return the_link.get('data').get('subscribers')
     except Exception:
@@ -17,3 +17,4 @@ def number_of_subscribers(subreddit):
 
 if __name__ == "__main__":
     number_of_subscribers(argv[1])
+
