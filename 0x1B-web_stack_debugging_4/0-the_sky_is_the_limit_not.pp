e7d9921ed9bbe0a4098711bc931d1badb2741257
HystERICKal <nyoroerick12@gmail.com>
2024-04-16 14:28:56 +0000

    Task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# fixing our stack
+
+exec {'fix nginx':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
